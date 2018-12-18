afe8faec8ce3a999a29e37621cd1ce05cf11cd7f
lucas-mauro <lucasmaurogil@gmail.com>
2018-12-18 17:38:33 -0300

    finish

--- src/main/java/com/in28minutes/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
+++ src/main/java/com/in28minutes/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -2,11 +2,20 @@ package com.in28minutes.springboot.basics.springbootin10steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import io.micrometer.core.instrument.MeterRegistry.Config;
 
 @SpringBootApplication
 public class SpringbootIn10StepsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+		ApplicationContext applicationContext = 
+				SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+		for (String name : applicationContext.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
 	}
 }
